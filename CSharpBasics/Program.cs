292fd773623b31ea3988fe66d434f524a33d38ec
utshaav025 <utshaav583@gmail.com>
2020-09-01 15:43:42 +0545

    First Commit of Net-centric computing

---
+++ CSharpBasics/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+            Console.WriteLine("Hello World!");
+            Console.Write("Enter your name: ");
+            string x = Console.ReadLine();
+            Console.WriteLine("YOu are handsome," + x);
+
+        }
+    }
+}
